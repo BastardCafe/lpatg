f7247462c68696798c1a99e54fe2cb5e141cee8a
Skjalm Arrøe <skjalm@gmail.com>
2017-03-27 12:14:21 +0200

    Changed to using jQuery Mobile Datebox for date input to fix date formatting issues. Fixes #4

--- inc/header.inc.php
+++ inc/header.inc.php
@@ -8,11 +8,13 @@ function GetHeader() {
 <title>' . $headTitle . '</title>
 <meta name="viewport" content="width=device-width, initial-scale=1" />
 <link rel="stylesheet" href="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css">
-<link rel="stylesheet" href="https://rawgithub.com/arschmitz/jquery-mobile-datepicker-wrapper/v0.1.1/jquery.mobile.datepicker.css" />
-<script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
+<link href="css/jtsage-datebox-4.1.1.jqm.min.css" rel="stylesheet" type="text/css">
+<script
+  src="https://code.jquery.com/jquery-1.12.4.min.js"
+  integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ="
+  crossorigin="anonymous"></script>
 <script src="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
-<script src="https://rawgithub.com/jquery/jquery-ui/1.10.4/ui/jquery.ui.datepicker.js"></script>
-<script id="mobile-datepicker" src="https://rawgithub.com/arschmitz/jquery-mobile-datepicker-wrapper/v0.1.1/jquery.mobile.datepicker.js"></script>
+<script src="js/jtsage-datebox-4.1.1.jqm.min.js" type="text/javascript"></script>
 ' . $style . '
 </head>
 <body>
--- www/gamelist.php
+++ www/gamelist.php
@@ -33,11 +33,13 @@ $db= GetDB();
 $query= "SELECT * FROM game WHERE id NOT IN (SELECT gameid FROM gameplay) ORDER BY title ASC";
 $dbres= $db->query($query);
 if ($dbres) {
+  /*
   $page.='<div data-role="fieldcontain"><label for="playedselect">Show played games</label>
     <select name="playedselect" id="playedselect" data-role="slider">
       <option value="no"></option>
       <option value="yes"></option>
-    </select></div>' . PHP_EOL;
+      </select></div>' . PHP_EOL;
+   */
   $page.='<ul data-role="listview" data-inset="true" data-autodividers="true" data-filter="true">' . PHP_EOL;
   while ($row= $dbres->fetch_assoc()) {
     $page.= '<li><a href="#" onClick="SetGameData('.$row['id'].','.$row['bbgid'].');">'.$row['title'].'</a></li>' . PHP_EOL;
@@ -45,6 +47,7 @@ if ($dbres) {
   $page.='</ul>' . PHP_EOL;
 }
 
+$currentDate= '"' . date('Y-m-d') . '"';
 $page.='
   </div>
 </div>
@@ -55,11 +58,16 @@ $page.='
     <form method="post" action="gameplayed.php">
       <h3>Player name: '.$user['name'].'</h3>
 <div>
-      <input type="hidden" name="gamedate" id="gamedate" data-role="date" data-inline="true" />
+<div>
+  <input name="gamedate" id="gamedate" data-role="datebox"
+  data-options=\'{"mode":"calbox","useInline":true,"showInitialValue":true,"overrideDateFormat":"%Y-%m-%d","calHighToday":false,"hideInput":true}\'/>
 </div>
+</div>
+<div>
+      <input type="submit" value="Submit" data-icon="check" data-iconpos="right" />
       <input type="hidden" name="gameid" id="gameid" />
       <input type="hidden" name="bggid" id="bggid" />
-      <input type="submit" value="Submit" data-icon="check" data-iconpos="right" />
+</div>
     </form>
   </div>
   <div data-role="collapsible" id="previousplaysheader">
@@ -100,11 +108,6 @@ $("#previousplays").append(\'<li><span class="ui-content">\' + item.gamedate + \
   });
 }
 $(document).ready(function() {
-  $.datepicker.setDefaults({
-    dateFormat: "yy-mm-dd",
-    minDate: "2017-03-01",
-    autoSize: true
-  });
   $("#playername").textinput("disable");
 });
 </script>
